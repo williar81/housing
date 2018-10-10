ef81b093eb538d10d6fe947202a86fde9822e317
Mario <140009@upslp.edu.mx>
2018-10-10 11:05:46 -0500

    Se inicia project

--- housing.py
+++ housing.py
@@ -1,3 +1,11 @@
-import numpy
+from sklearn import datasets
+import numpy as np
 
-print("Hello World")
+data_set = datasets.load_boston()
+data, target = data_set.data, data_set.target
+
+np.delete(data, [1, 10], axis=1)
+new_data = data[:, :-1]
+
+print(new_data.shape)
+# print(target)
