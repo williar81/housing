9e4bdc8ab1506f70ac73434f590b24e92ba18393
Mario <140009@upslp.edu.mx>
2018-10-11 23:36:29 -0500

    Se termina esta wea ya chido

--- housing.py
+++ housing.py
@@ -30,7 +30,7 @@ environment.reproducible(47)
 # Separamos datos de entrena y prueba a un 70%
 x_train, x_test, y_train, y_test = train_test_split(
     data_scaler.fit_transform(data),
-    data_scaler.fit_transform(target.reshape(-1, 1)),
+    target_scaler.fit_transform(target.reshape(-1, 1)),
     train_size=0.70
 )
 
@@ -48,10 +48,13 @@ red = algorithms.ConjugateGradient(
 # Entrenamos la red neuronal
 red.train(x_train, y_train, x_test, y_test, epochs=100)
 
-# Desnormalizamos datos y calculamos error
+# Guardamos las predicciones en variable
 y_predict = red.predict(x_test).round(1)
 
-error = rmsle(target_scaler.inverse_transform(y_test),
-              target_scaler.inverse_transform(y_predict))
+# Desnormalizamos datos
+real = target_scaler.inverse_transform(y_test)
+predicted = target_scaler.inverse_transform(y_predict)
 
-print(error)
+# Calculamos error
+error = rmsle(real, predicted)
+print("ECM:", error)
